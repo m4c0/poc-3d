2b965ae9d4393f4f9b86953801d0e3217a21fd88
Eduardo Costa <m4c0@users.noreply.github.com>
2026-01-06 17:20:50 +0000

    axis: using a larger face

--- poc-axis.cpp
+++ poc-axis.cpp
@@ -41,13 +41,13 @@ struct app_stuff : vinyl::base_app_stuff {
 
   app_stuff() : base_app_stuff { "poc-3d" } {
     auto m = vbuf.map();
-    m += vtx { .pos { -0.5, -0.5, 0.5, 1.0 } };
-    m += vtx { .pos {  0.5, -0.5, 0.5, 1.0 } };
-    m += vtx { .pos { -0.5,  0.5, 0.5, 1.0 } };
+    m += vtx { .pos { -0.9, -0.9, 0.9, 1.0 } };
+    m += vtx { .pos {  0.9, -0.9, 0.9, 1.0 } };
+    m += vtx { .pos { -0.9,  0.9, 0.9, 1.0 } };
 
-    m += vtx { .pos {  0.5,  0.5, 0.5, 1.0 } };
-    m += vtx { .pos {  0.5, -0.5, 0.5, 1.0 } };
-    m += vtx { .pos { -0.5,  0.5, 0.5, 1.0 } };
+    m += vtx { .pos {  0.9,  0.9, 0.9, 1.0 } };
+    m += vtx { .pos {  0.9, -0.9, 0.9, 1.0 } };
+    m += vtx { .pos { -0.9,  0.9, 0.9, 1.0 } };
   }
 };
 struct ext_stuff : vinyl::base_extent_stuff {
